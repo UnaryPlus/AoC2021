621a70a8809decf5130f496b9119d73096ee4b8e
UnaryPlus <ombspring@gmail.com>
2021-12-04 16:26:50 -0500

    create repository

---
+++ Main.hs
@@ -0,0 +1,257 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE TupleSections #-}
+
+module Main where
+
+import qualified Data.Maybe as Maybe
+import qualified Data.Char as Char
+import qualified Data.List as List
+import qualified Text.Read as Read
+import qualified Debug.Trace as Debug
+
+main :: IO ()
+main = do
+  contents <- readFile "giant-squid.txt"
+  print (giantSquid2 contents)
+
+--------------------------
+--- DAY 1: SONAR SWEEP ---
+--------------------------
+
+trim :: String -> String
+trim = let
+  f = reverse . dropWhile Char.isSpace
+  in f . f
+
+readTrim :: String -> Maybe Int
+readTrim = Read.readMaybe . trim
+
+pairs :: [a] -> [(a, a)]
+pairs [] = []
+pairs [_] = []
+pairs (x:y:rest) = (x, y) : pairs (y:rest)
+
+triples :: [a] -> [(a, a, a)]
+triples [] = []
+triples [_] = []
+triples [_, _] = []
+triples (x:y:z:rest) = (x, y, z) : triples(y:z:rest)
+
+count :: (a -> Bool) -> [a] -> Int
+count f = length . filter f
+
+sonarSweep1 :: String -> Int
+sonarSweep1 file = let
+  nums = Maybe.mapMaybe readTrim (lines file)
+  ordered = uncurry (<)
+  in count ordered (pairs nums)
+
+sonarSweep2 :: String -> Int
+sonarSweep2 file = let
+  nums = Maybe.mapMaybe readTrim (lines file)
+  ordered = uncurry (<)
+  sum3 (x, y, z) = x + y + z
+  windows = map sum3 (triples nums)
+  in count ordered (pairs windows)
+
+-------------------
+--- DAY 2: DIVE ---
+-------------------
+
+headMaybe [] = Nothing
+headMaybe (x:_) = Just x
+
+tailMaybe [] = Nothing
+tailMaybe (_:xs) = Just xs
+
+data Direction = Up | Down | Forward deriving (Eq)
+
+readDirection :: String -> Maybe Direction
+readDirection "up" = Just Up
+readDirection "down" = Just Down
+readDirection "forward" = Just Forward
+readDirection _ = Nothing
+
+readCommand :: String -> Maybe (Direction, Int)
+readCommand line = do
+  let parts = words line
+  dirStr <- headMaybe parts
+  numStr <- headMaybe =<< tailMaybe parts
+  dir <- readDirection dirStr
+  num <- Read.readMaybe numStr
+  return (dir, num)
+
+sumDirection :: Direction -> [(Direction, Int)] -> Int
+sumDirection _ [] = 0
+sumDirection toSum ((dir, num) : rest)
+  | dir == toSum = num + sumDirection toSum rest
+  | otherwise = sumDirection toSum rest
+
+dive1 :: String -> Int
+dive1 file = let
+  commands = Maybe.mapMaybe readCommand (lines file)
+  ups = sumDirection Up commands
+  downs = sumDirection Down commands
+  pos = sumDirection Forward commands
+  depth = downs - ups
+  in pos * depth
+
+updatePosition :: (Int, Int, Int) -> (Direction, Int) -> (Int, Int, Int)
+updatePosition (aim, pos, depth) (dir, num) =
+  case dir of
+    Up -> (aim - num, pos, depth)
+    Down -> (aim + num, pos, depth)
+    Forward -> (aim, pos + num, depth + aim * num)
+
+dive2 :: String -> Int
+dive2 file = let
+  commands = Maybe.mapMaybe readCommand (lines file)
+  (_, pos, depth) = foldl updatePosition (0, 0, 0) commands
+  in pos * depth
+
+--------------------------------
+--- DAY 3: BINARY DIAGNOSTIC ---
+--------------------------------
+
+type BinaryRow = [Bool]
+
+readBinaryRow :: String -> Maybe BinaryRow
+readBinaryRow = let
+  readBit '0' = Just False
+  readBit '1' = Just True
+  readBit _ = Nothing
+  in mapM readBit . trim
+
+modeBool :: [Bool] -> Bool
+modeBool bs = let
+  (ts, fs) = List.partition id bs
+  in length ts >= length fs
+
+columnModes :: [BinaryRow] -> BinaryRow
+columnModes = map modeBool . List.transpose
+
+rowToInt :: BinaryRow -> Int
+rowToInt = let
+  build _ [] = 0
+  build factor (x:xs) = factor * fromEnum x + build (factor * 2) xs
+  in build 1 . reverse
+
+binaryDiagnostic1 :: String -> Int
+binaryDiagnostic1 file = let
+  rows = Maybe.mapMaybe readBinaryRow (lines file)
+  gamma = columnModes rows
+  epsilon = map not gamma
+  in rowToInt gamma * rowToInt epsilon
+
+getMaybe :: Int -> [a] -> Maybe a
+getMaybe _ [] = Nothing
+getMaybe 0 (x:_) = Just x
+getMaybe index (x:xs) = getMaybe (index - 1) xs
+
+matchesAt :: (Eq a) => Int -> a -> [a] -> Bool
+matchesAt index test list
+  | Just value <- getMaybe index list = value == test
+  | otherwise = False
+
+filterRowsBy :: (Bool -> Bool) -> Int -> [BinaryRow] -> Maybe [BinaryRow]
+filterRowsBy f index rows = do
+  col <- mapM (getMaybe index) rows
+  let mode = f (modeBool col)
+  return $ filter (matchesAt index mode) rows
+
+extractRowBy :: (Bool -> Bool) -> Int -> [BinaryRow] -> Maybe BinaryRow
+extractRowBy f index rows = do
+  matching <- filterRowsBy f index rows
+  case matching of
+    [] -> Nothing
+    [row] -> Just row
+    _ -> extractRowBy f (index + 1) matching
+
+binaryDiagnostic2 :: String -> Int
+binaryDiagnostic2 file = let
+  rows = Maybe.mapMaybe readBinaryRow (lines file)
+  modes = columnModes rows
+  extract f = Maybe.fromMaybe [] (extractRowBy f 0 rows)
+  oxygen = extract id
+  carbon = extract not
+  in rowToInt oxygen * rowToInt carbon
+
+--------------------------
+--- DAY 4: GIANT SQUID ---
+--------------------------
+
+type BingoBoard = [[(Int, Bool)]]
+
+spaceSepNums :: String -> [Int]
+spaceSepNums = Maybe.mapMaybe Read.readMaybe . words
+
+commaSepNums :: String -> [Int]
+commaSepNums = let
+  replace ',' = ' '
+  replace c = c
+  in spaceSepNums . map replace
+
+validBoard :: BingoBoard -> Bool
+validBoard board = let
+  length5 xs = length xs == 5
+  in length5 board && all length5 board
+
+bingoBoards :: [Int] -> [BingoBoard]
+bingoBoards nums = let
+  split5 [] = []
+  split5 xs = take 5 xs : split5 (drop 5 xs)
+  tables = split5 (split5 nums)
+  boards = (map . map . map) (, False) tables
+  in take 100 boards
+
+playRound :: Int -> [BingoBoard] -> [BingoBoard]
+playRound num boards = let
+  mark (x, _) | x == num = (x, True)
+  mark square = square
+  in (map . map . map) mark boards
+
+isWinner :: BingoBoard -> Bool
+isWinner board = let
+  winningRow row = all snd row
+  in any winningRow board || any winningRow (List.transpose board)
+
+findWinner :: [BingoBoard] -> Maybe BingoBoard
+findWinner = headMaybe . filter isWinner
+
+scoreBingo :: Int -> BingoBoard -> Int
+scoreBingo num board = let
+  getUnmarked (x, False) = Just x
+  getUnmarked _ = Nothing
+  unmarked = concatMap (Maybe.mapMaybe getUnmarked) board
+  in sum unmarked * num
+
+playBingo :: [Int] -> [BingoBoard] -> Maybe Int
+playBingo [] _ = Nothing
+playBingo (num : nums) boards = let
+  boards' = playRound num boards
+  in case findWinner boards' of
+    Nothing -> playBingo nums boards'
+    Just winner -> Just (scoreBingo num winner)
+
+giantSquid1 :: String -> Int
+giantSquid1 file = let
+  (headLine, tailLines) = Maybe.fromMaybe ("", []) $ List.uncons (lines file)
+  nums = commaSepNums headLine
+  boards = bingoBoards $ spaceSepNums (unwords tailLines)
+  in Maybe.fromMaybe 0 (playBingo nums boards)
+
+playReverseBingo :: [Int] -> [BingoBoard] -> Maybe Int
+playReverseBingo [] _ = Nothing
+playReverseBingo (num : nums) boards = let
+  boards' = playRound num boards
+  in case filter (not . isWinner) boards' of
+    [] -> Nothing
+    [loser] -> playBingo nums [loser]
+    losers -> playReverseBingo nums losers
+
+giantSquid2 :: String -> Int
+giantSquid2 file = let
+  (headLine, tailLines) = Maybe.fromMaybe ("", []) $ List.uncons (lines file)
+  nums = commaSepNums headLine
+  boards = bingoBoards $ spaceSepNums (unwords tailLines)
+  in Maybe.fromMaybe 0 (playReverseBingo nums boards)
